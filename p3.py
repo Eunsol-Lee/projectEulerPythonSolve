39679e557396ed40ca9560bf71276588503555ab
Eunsol Lee <eunsol@Eunsols-MacBook-Pro.local>
2017-01-09 00:11:28 +0900

    1~3

---
+++ p3.py
@@ -0,0 +1,19 @@
+import math
+
+N = 600851475143
+prime = [2];
+all = [1] * int(math.sqrt(N))
+
+for i in range(3, int(math.sqrt(N)), 2):
+    if all[i]:
+        for j in range(i * 3, int(math.sqrt(N)), i * 2):
+            all[j] = 0
+        prime.append(i)
+
+for i in range(len(prime)):
+    if N % prime[i] == 0:
+        N /= prime[i]
+        print(prime[i])
+        continue
+
+print (N)
